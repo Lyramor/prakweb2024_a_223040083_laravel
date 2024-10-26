9448e2ec9f7f506bad833a583869076722f3b2dc
Lyramor <mmarsa2435@gmail.com>
2024-10-27 04:26:07 +0700

    Video 11 - Eloquent Relationship

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
@@ -21,4 +22,8 @@ Route::get('/contact', function () {
 
 Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/authors/{user}', function (User $user) {
+    return view('posts', ['title' => 'Articles by ' . $user->name, 'posts' => $user->posts]);
 });
