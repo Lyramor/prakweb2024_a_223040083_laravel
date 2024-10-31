bd9032b77cd2364b85ea31ec567abf0b79c5543a
Lyramor <mmarsa2435@gmail.com>
2024-11-01 01:01:30 +0700

    Video 19_15 - User Registration

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,11 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
     public function index(){
         return view('register.index', ['title' => 'Register']);
+    }
+
+    public function store(Request $request){
+        $validateData = $request->validate([
+            'name'=>'required|max:255',
+            'username'=>['required', 'min:2', 'max:255', 'unique:users'],
+            'email'=>'required|email:dns|unique:users',
+            'password'=>'required|min:8|max:255'
+        ]);
+
+        //$validateData['password'] = bcrypt($validateData['password']);
+
+        $validateData['password'] = Hash::make($validateData['password']);
+
+        User::create($validateData);
+        // $request->session()->flash('success', 'Registration successfull! Please Login');
+
+        return redirect('/login')->with('success', 'Registration successfull! Please Login');
     }
 }
--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -1,28 +1,6 @@
 <x-layout>
   <x-slot:title>{{ $title }}</x-slot:title>
 
-    <!--
-    This example requires some changes to your config:
-    
-    ```
-    // tailwind.config.js
-    module.exports = {
-      // ...
-      plugins: [
-        // ...
-        require('@tailwindcss/forms'),
-      ],
-    }
-    ```
-  -->
-  <!--
-    This example requires updating your template:
-
-    ```
-    <html class="h-full bg-white">
-    <body class="h-full">
-    ```
-  -->
   <div class="col-md-5">
     <main class="form-registration">
       <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
@@ -36,47 +14,64 @@
         </div>
     
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form class="space-y-6" action="#" method="POST">
+          <form class="space-y-6" action="/register" method="POST">
+            @csrf
+
+            <!-- Field name -->
             <div>
-              <label for="name" class="block text-sm/6 font-medium text-gray-900">Name</label>
+              <label for="name" class="block text-sm font-medium text-gray-900">Name</label>
               <div class="mt-2">
-                <input id="name" name="name" type="name" autocomplete="name" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6">
+                <input id="name" name="name" type="text" required value="{{ old('name') }}" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm">
+                @error('name')
+                  <p class="text-red-600 text-sm mt-1">{{ $message }}</p>
+                @enderror
               </div>
             </div>
+
+            <!-- Field username -->
             <div>
-              <label for="username" class="block text-sm/6 font-medium text-gray-900">Username</label>
+              <label for="username" class="block text-sm font-medium text-gray-900">Username</label>
               <div class="mt-2">
-                <input id="username" name="username" type="text" autocomplete="name" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6">
+                <input id="username" name="username" type="text" required value="{{ old('username') }}" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm">
+                @error('username')
+                  <p class="text-red-600 text-sm mt-1">{{ $message }}</p>
+                @enderror
               </div>
             </div>
+
+            <!-- Field email -->
             <div>
-              <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
+              <label for="email" class="block text-sm font-medium text-gray-900">Email address</label>
               <div class="mt-2">
-                <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6">
+                <input id="email" name="email" type="email" required value="{{ old('email') }}" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm">
+                @error('email')
+                  <p class="text-red-600 text-sm mt-1">{{ $message }}</p>
+                @enderror
               </div>
             </div>
+
+            <!-- Field password -->
             <div>
-              <div class="flex items-center justify-between">
-                <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
-              </div>
+              <label for="password" class="block text-sm font-medium text-gray-900">Password</label>
               <div class="mt-2">
-                <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6">
+                <input id="password" name="password" type="password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm">
+                @error('password')
+                  <p class="text-red-600 text-sm mt-1">{{ $message }}</p>
+                @enderror
               </div>
             </div>
-    
+
             <div>
-              <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Register</button>
+              <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Register</button>
             </div>
           </form>
-          <p class="mt-10 text-center text-sm/6 text-gray-500">
-            Allready registed?
+
+          <p class="mt-10 text-center text-sm text-gray-500">
+            Already registered?
             <a href="/login" class="font-semibold text-indigo-600 hover:text-indigo-500">Login</a>
           </p>
-    
         </div>
       </div>
     </main>
   </div>
-  
-  
 </x-layout>
