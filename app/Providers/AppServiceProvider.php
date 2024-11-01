95153b3bf063c68792914104f6edbb447e14a0d9
Lyramor <mmarsa2435@gmail.com>
2024-11-01 10:53:24 +0700

    Video 20_16 - User Login & Middleware

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,8 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    // public const HOME = '/home';
+    public const HOME = '/';
     /**
      * Register any application services.
      */
---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,1 @@
+<h1>Welcome Muhamad Marsa Nur Jaman</h1>
